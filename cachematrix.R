1b010997869439b4fe67ae4f72721c2e46c1cff3
Windy024 <aravind.be.mechengg@gmail.com>
2019-12-01 18:03:45 +0530

    This is my workings

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are going to be 2 functions. 1 that returns a list of 4 functions and stores the inverse of a matrix in cache the other that returns the inverse of matrix 
+## If the inverse of a particular matrix has already been calculated, the inverse is returned from the cache
 
-## Write a short comment describing this function
-
+## This is the first function that stores the inverse of the matrix in cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y #'<<-' is used to assign a value to an object in an environment that is different from the current environment
+    inv <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setInverse <- function(solveMatrix) {
+    inv <<- solveMatrix
+    }
+  getInverse <- function() {
+    inv
+  }
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function checks whether the inverse of the matrix is already stored in cache and returns the inverse from the cache or by calculating it using the solve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("This was received from cached data")
+    return(inv)
+  }
+  message("This was received as a result of the solve function")
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv
 }
+
+#TESTING THE FUNCTION
+a <- matrix(1:4,2) 
+a
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+test <- makeCacheMatrix(a) #This returns a list of 4 functions
+cacheSolve(test)
+#This was received as a result of the solve function
+#      [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+cacheSolve(test)
+#This was received from cached data
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
